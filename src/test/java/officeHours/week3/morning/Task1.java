916d11c6ea4d9183952ef9d1eb1e76d8159eb269
arstan <arstankmn@gmail.com>
2023-09-01 11:57:45 -0500

    added Random method and created new region id and name tested

--- src/test/java/officeHours/week3/morning/Task1.java
+++ src/test/java/officeHours/week3/morning/Task1.java
@@ -2,28 +2,55 @@ package officeHours.week3.morning;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import officeHours.week3.morning.pojo.Region;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import utilities.HrTestBase;
+import io.restassured.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 
 public class Task1  extends HrTestBase {
 
     @Test
     void create_region() {
         Map<String, Object> RequestBody =new HashMap<>();
-        RequestBody.put("region_id", 364);
+        RequestBody.put("region_id", 555);
         RequestBody.put("region_name","Test region");
 
+/* we tried MAP and POJO*/
+        Random random = new Random();
+        int random_region_id = random.nextInt(1000)+100;
+        int expected_region_id = random_region_id;
+        String expected_region_name ="Arstan region";
+        System.out.println("expected_region_id = " + expected_region_id);
 
 
-        RestAssured
+        Response response = RestAssured
                 .given()
                 .accept(ContentType.JSON)
                 .contentType(ContentType.JSON)
-                .body(RequestBody)
+
+                .body(new Region(random_region_id, expected_region_name))
                 .post("/regions/").prettyPeek();
+
+        int actual_region_id = response.jsonPath().getInt("region_id");
+        String actual_region_name = response.jsonPath().getString("region_name");
+        System.out.println("actual_region_id = " + actual_region_id);
+        Assertions.assertEquals(expected_region_id,actual_region_id);
+        Assertions.assertEquals(expected_region_name, expected_region_name);
+
+
+    }
+
+    @Test
+    void get_region() {
+
+
+
     }
 }
